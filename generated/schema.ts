4499168cb614e70b70ba6ed854fb603cf6a91b16
Siegi Skalla <s.skalla@cryptix.ag>
2025-02-05 13:38:41 +0100

    Total swaps

--- generated/schema.ts
+++ generated/schema.ts
@@ -119,3 +119,56 @@ export class SwapEvent extends Entity {
     this.set("blockNumber", Value.fromBigInt(value));
   }
 }
+
+export class TotalStats extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id != null, "Cannot save TotalStats entity without an ID");
+    if (id) {
+      assert(
+        id.kind == ValueKind.STRING,
+        `Entities of type TotalStats must have an ID of type String but the id '${id.displayData()}' is of type ${id.displayKind()}`,
+      );
+      store.set("TotalStats", id.toString(), this);
+    }
+  }
+
+  static loadInBlock(id: string): TotalStats | null {
+    return changetype<TotalStats | null>(store.get_in_block("TotalStats", id));
+  }
+
+  static load(id: string): TotalStats | null {
+    return changetype<TotalStats | null>(store.get("TotalStats", id));
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    if (!value || value.kind == ValueKind.NULL) {
+      throw new Error("Cannot return null for a required field.");
+    } else {
+      return value.toString();
+    }
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get total(): BigInt {
+    let value = this.get("total");
+    if (!value || value.kind == ValueKind.NULL) {
+      throw new Error("Cannot return null for a required field.");
+    } else {
+      return value.toBigInt();
+    }
+  }
+
+  set total(value: BigInt) {
+    this.set("total", Value.fromBigInt(value));
+  }
+}
--- src/mapping.ts
+++ src/mapping.ts
@@ -1,5 +1,6 @@
 import { Swapped } from '../generated/SwapContract/SwapContract'
-import { SwapEvent } from '../generated/schema'
+import { SwapEvent, TotalStats } from '../generated/schema'
+import { BigInt } from "@graphprotocol/graph-ts"
 
 export function handleSwapped(event: Swapped): void {
   let swapEvent = new SwapEvent(event.transaction.hash.concatI32(event.logIndex.toI32()))
@@ -11,4 +12,14 @@ export function handleSwapped(event: Swapped): void {
   swapEvent.blockNumber = event.block.number
   
   swapEvent.save()
+
+  const totalSwapped = "totalActAmountSwapped";
+  let stats = TotalStats.load(totalSwapped)
+  if (!stats) {
+    stats = new TotalStats(totalSwapped)
+    stats.total = BigInt.fromI32(0);
+  }
+  
+  stats.total = stats.total.plus(event.params.actAmount)
+  stats.save()
 }
